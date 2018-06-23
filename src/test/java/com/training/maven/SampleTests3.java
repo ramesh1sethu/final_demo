f2ac022ee0a10304b8db62ccba505f56f0f9f0eb
praveen <praveensamala.tcs@gmail.com>
2018-06-23 13:54:59 +0530

    another update

---
+++ src/test/java/com/training/maven/SampleTests3.java
@@ -0,0 +1,30 @@
+package com.training.maven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests3 {
+	@Test
+	public void testcase01() {
+		System.out.println("*****IN TESTCASE01");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testcase02() {
+		System.out.println("*****IN TESTCASE02");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testcase03() {
+		System.out.println("*****IN TESTCASE03");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testcase04() {
+		System.out.println("*****IN TESTCASE04");
+		Assert.assertTrue(true);
+	}
+}
