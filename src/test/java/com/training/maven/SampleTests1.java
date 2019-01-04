ab950f2132fef60b0da09013f98fb45c8d300c54
CVRAMAN-536 <CVRAMAN-536@192.168.40.28>
2019-01-04 17:00:42 +0530

    Card_payment-added comments on all the classes

--- src/test/java/com/training/maven/SampleTests1.java
+++ src/test/java/com/training/maven/SampleTests1.java
@@ -3,25 +3,41 @@ package com.training.maven;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+/**
+ * @author CVRAMAN-536
+ *
+ */
 public class SampleTests1 {
+	/**
+	 * 
+	 */
 	@Test
 	public void testcase01() {
 		System.out.println("*****IN TESTCASE01");
 		Assert.assertTrue(true);
 	}
 
+	/**
+	 * 
+	 */
 	@Test
 	public void testcase02() {
 		System.out.println("*****IN TESTCASE02");
 		Assert.assertTrue(true);
 	}
 
+	/**
+	 * 
+	 */
 	@Test
 	public void testcase03() {
 		System.out.println("*****IN TESTCASE03");
 		Assert.assertTrue(true);
 	}
 
+	/**
+	 * 
+	 */
 	@Test
 	public void testcase04() {
 		System.out.println("*****IN TESTCASE04");
