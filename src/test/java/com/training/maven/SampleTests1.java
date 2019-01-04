401781d82f7e4537c0dacb18de72b7c68278a249
CVRAMAN-535 <CVRAMAN-535@192.168.40.51>
2019-01-04 16:58:09 +0530

    Revert "added testcase00"
    This reverts commit 391692449aa5681092719d62700a5ca13d97df35.

--- src/test/java/com/training/maven/SampleTests1.java
+++ src/test/java/com/training/maven/SampleTests1.java
@@ -4,12 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SampleTests1 {
-	
-	@Test
-	public void testcase00() {
-		System.out.println("*****IN TESTCASE0000");
-		Assert.assertTrue(true);
-	}
 	@Test
 	public void testcase01() {
 		System.out.println("*****IN TESTCASE01");
