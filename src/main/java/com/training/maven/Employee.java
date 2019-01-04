766efab0941feaa70300323ab3fffb9db4042eb5
CVRAMAN-536 <CVRAMAN-536@192.168.40.28>
2019-01-04 17:51:09 +0530

    card_payment-created new Employee.class

---
+++ src/main/java/com/training/maven/Employee.java
@@ -0,0 +1,37 @@
+package com.training.maven;
+
+/**
+ * @author CVRAMAN-536
+ *
+ */
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	/**
+	 * @return the empId
+	 */
+	public int getEmpId() {
+		return empId;
+	}
+	/**
+	 * @param empId the empId to set
+	 */
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+	/**
+	 * @param empName the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+
+}
