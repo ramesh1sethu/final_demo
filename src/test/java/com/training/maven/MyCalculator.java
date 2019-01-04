8a79b66367d0eeea2b70e0a723a2f3b6376de6cc
CVRAMAN-536 <CVRAMAN-536@192.168.40.28>
2019-01-04 17:04:46 +0530

    card_payment-created a MyCalculator class having addition method.

---
+++ src/test/java/com/training/maven/MyCalculator.java
@@ -0,0 +1,19 @@
+package com.training.maven;
+
+/**
+ * @author CVRAMAN-536
+ *
+ */
+public class MyCalculator {
+
+	/**
+	 * @param inputNumber1
+	 * @param inputNumber2
+	 * @return outputNumber
+	 */
+	public static int addition(int inputNumber1, int inputNumber2) {
+		int outputNumber = inputNumber1 + inputNumber2;
+		return outputNumber;
+	}
+
+}
