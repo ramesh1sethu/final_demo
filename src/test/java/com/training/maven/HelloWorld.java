1b7cf08df71b6e621fe8c2236f26a2cff1c798f5
CVRAMAN-485 <CVRAMAN-485@192.168.40.39>
2019-01-04 17:03:03 +0530

    I have add new class

---
+++ src/test/java/com/training/maven/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.training.maven;
+
+import java.util.logging.*;
+
+//This is a demo 
+public class HelloWorld {
+
+	public static final Logger logger = Logger.getLogger("HelloWorld.class");
+
+	public static void main(String[] args) {
+		logger.log(Level.INFO, "Hello World program started.");
+
+		for (int count = 0; count<5; count++) {
+			logger.log(Level.INFO, "Value is {0}", count);
+			 logger.info("changed  by Tiwari");
+			 System.out.println("vids");
+		}
+
+	}
+
+}
