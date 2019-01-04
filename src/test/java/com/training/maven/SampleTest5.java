41eb60c9791bc386edfd80ff1aac225d6b5410e4
CVRAMAN-484 <CVRAMAN-484@192.168.40.54>
2019-01-04 16:59:53 +0530

    i have added new class SampleTest5.

---
+++ src/test/java/com/training/maven/SampleTest5.java
@@ -0,0 +1,13 @@
+package com.training.maven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest5 {
+	@Test
+	public void testcase01() {
+		System.out.println("*****IN CASE01");
+		Assert.assertTrue(true);
+	}
+
+}
